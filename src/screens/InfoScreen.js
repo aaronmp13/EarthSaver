b3dcce18bd27781cee69d481679f5dd39e265fcd
GeneralRoasterock <roasterock@gmail.com>
2021-10-12 15:32:54 -0400

    add

--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -5,7 +5,7 @@ import Firebase from '../firebase/config';
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#F2F2F2',
       alignItems: 'center',
       justifyContent: 'center',
     },
