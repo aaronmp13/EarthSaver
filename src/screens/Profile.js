25267539867e228de5a6b7a5778ae5588762dd50
GeneralRoasterock <roasterock@gmail.com>
2021-10-12 17:04:58 -0400

    profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,24 +3,43 @@ import { StyleSheet, Text, View } from 'react-native';
 import Firebase from '../firebase/config';
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#F2F2F2',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    profile: {
+  container: {
+    flex: 1,
+    backgroundColor: '#F2F2F2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profile: {
 
-    }
-
-  });
+  },
+  profileText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    alignItems: "center",
+    color: "#000"
+  },
+  settingsText: {
+    fontSize: 12,
+    color: "#000"
+  },
+  rankText: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "#000"
+  },
+  labelText: {
+    fontSize: 12,
+    fontWeight: "bolb",
+    color: "#808080"
+  },
+});
 
   export default function Profile() {
     return (
       <View style={styles.container}>
         <StatusBar style='light-content' />
         <View style={styles.profile}>
-          <Text style={styles.title}>{user}</Text>
+          <Text style={styles.profileText}>{user}</Text>
         </View>
         <Text style={styles.text}></Text>
       </View>
