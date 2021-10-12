fd2f7b63d5303f1cd1814d23f0fcbe66c426eb6b
GeneralRoasterock <roasterock@gmail.com>
2021-10-12 14:37:55 -0400

    info screen framework

---
+++ src/screens/InfoScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Firebase from '../firebase/config';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default function infoScreen() {
+    return (
+      <View style={styles.container}>
+        <StatusBar style='dark-content' />
+        <View style={styles.row}>
+          <Text style={styles.title}>How to Earn Points:</Text>
+        </View>
+        <Text style={styles.text}> When you submit an activity to the EarthSaver app, you can earn points for your account. These points
+        will be given out after validation by a member of the team. Specific point values are set for certain activities, but events that
+        have a greater impact on the environment generally are worth more.</Text>
+      </View>
+    );
+  }
