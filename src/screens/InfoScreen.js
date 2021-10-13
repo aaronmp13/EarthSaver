3c791d3e212038ad905e35e798529b2f4b24cfc2
Aaron Ponnraj <amp7yqb@virginia.edu>
2021-10-13 16:04:09 -0400

    Update InfoScreen.js
    update

--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -5,7 +5,7 @@ import Firebase from '../firebase/config';
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#F2F2F2',
+      backgroundColor: '#F2F2F1',
       alignItems: 'center',
       justifyContent: 'center',
     },
